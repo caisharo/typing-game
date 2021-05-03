233bad5fa4ca63c149cb20d289f87fe66fd6e281
Sharon <caisharo@cs.washington.edu>
2021-05-03 04:22:42 -0700

    Updated PlayState.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,13 +7,17 @@ class PlayState extends FlxState
 	var hud:HUD;
 	var day:Int;
 	var money:Int;
+	var playerInput:PlayerInput;
 
 	override public function create()
 	{
 		super.create();
 
-		var hud = new HUD();
+		hud = new HUD();
 		add(hud);
+
+		playerInput = new PlayerInput();
+		add(playerInput);
 	}
 
 	override public function update(elapsed:Float)

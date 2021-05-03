e17392bc42521fe817269186d89b9368da1c2562
Shelly <shellyc@cs.washington.edu>
2021-05-03 02:16:26 -0700

    Basic money (score) and days passed HUD

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,9 +4,16 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var hud:HUD;
+	var day:Int;
+	var money:Int;
+
 	override public function create()
 	{
 		super.create();
+
+		var hud = new HUD();
+		add(hud);
 	}
 
 	override public function update(elapsed:Float)

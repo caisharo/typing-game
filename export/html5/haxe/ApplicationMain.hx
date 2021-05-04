aeaa34bf6dfb92f9287977b65319baccd08e289c
Sharon <caisharo@cs.washington.edu>
2021-05-04 12:37:21 -0700

    Added something to Order.txt. (Really just needed to push something to test the github workflow)

--- export/html5/haxe/ApplicationMain.hx
+++ export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "51";
+		app.meta["build"] = "53";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "TypingGame";
 		app.meta["name"] = "TypingGame";

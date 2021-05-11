7f8ef582758239ceb0bffdb31c4938c55d5a85c1
Sharon <caisharo@cs.washington.edu>
2021-05-11 11:15:47 -0700

    Changed category ID for today's playtesting.

--- source/Main.hx
+++ source/Main.hx
@@ -17,11 +17,12 @@ class Main extends Sprite
 		var gameId:Int = 202105;
 		var gameName:String = "typing";
 		var gameKey:String = "79a131f53a8376e7c93e04af79a0b81c";
-		var categoryId:Int = 1;
+		var categoryId:Int = 4;
 		// categoryId: A number you assign to help categorize data from different releases for your game.
 		// debugging = 1
 		// release to family/friends = 2,
-		// release it to a public website = 3.
+		// release it to a public website = 3
+		// 5/11 playtesting = 4
 		// IMPORTANT: Change this when you have a new version of your game! (We will probably increase by 10 each version?)
 
 		if (Main.isLogging)
